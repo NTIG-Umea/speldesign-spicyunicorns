489d14d4397222a0546f60c1e26a773e3ba0070a
KOSSAN38 <isak.edlund@elev.ga.ntig.se>
2020-11-24 10:48:21 +0100

    started on enemyplacer

---
+++ EnemyPlacer.js
@@ -0,0 +1,18 @@
+
+
+enemyplacer(lane1, lane2, lane3, lane4); {
+
+    var value = Phaser.Math.Between(1, 4);
+
+    if (value = 1)
+        return lane1;
+
+    if (value = 2)
+        return lane2;
+
+    if (value = 3)
+        return lane3;
+
+    if (value = 4)
+        return lane4;
+}
