6a16e1aab7c0fed2ecd5b4fa5eac5a97aa4f5a10
ChristianBackstrom <christian.backstrom@elev.ga.ntig.se>
2020-11-24 10:10:04 +0100

    movement with delay

--- mainScene.js
+++ mainScene.js
@@ -10,37 +10,49 @@ class mainScene extends Phaser.Scene{
         this.lane2 = 300;
         this.lane3 = 500;
         this.lane4 = 700;
+        this.delay = 15;
+        this.currentDelay = 0;
     }
 
     create(){
         this.player = this.add.sprite(300, 400, 'player');
-        this.player.x = this.lane2
+        this.player.x = this.lane3
 
         this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     moveLeft(){
-        if (this.player.x == this.lane2){
-            this.player.x == this.lane1;
+        if (this.currentDelay == 0){
+
+            if (this.player.x == this.lane2){
+                this.player.x = this.lane1;
+                console.log("hej");
+            }
+            if (this.player.x == this.lane3){
+                this.player.x = this.lane2;
+                console.log("hej");
+            }
+            if (this.player.x == this.lane4){
+                this.player.x = this.lane3;
+                console.log("hej");
+            }
+            this.currentDelay = this.delay;
         }
-        if (this.player.x == this.lane3){
-            this.player.x == this.lane2;
-        }
-        if (this.player.x == this.lane4){
-            this.player.x == this.lane3;
-        }
-        console.log("hej");
     }
 
     moveRight(){
-        if (this.player.x == this.lane3){
-            this.player.x == this.lane4;
-        }
-        if (this.player.x == this.lane2){
-            this.player.x == this.lane3;
-        }
-        if (this.player.x == this.lane1){
-            this.player.x == this.lane2;
+        if (this.currentDelay == 0){
+            
+            if (this.player.x == this.lane3){
+                this.player.x = this.lane4;
+            }
+            if (this.player.x == this.lane2){
+                this.player.x = this.lane3;
+            }
+            if (this.player.x == this.lane1){
+                this.player.x = this.lane2;
+            }
+            this.currentDelay = this.delay;
         }
     }
 
@@ -50,6 +62,9 @@ class mainScene extends Phaser.Scene{
         }
         if (this.cursors.right.isDown){
             this.moveRight();
+        }
+        if (this.currentDelay > 0){
+            this.currentDelay--;
         }
     }
 }
