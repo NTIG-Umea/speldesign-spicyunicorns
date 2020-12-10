4dfc34bf44a8512b5a581755f31d512b6319c664
KOSSAN38 <isak.edlund@elev.ga.ntig.se>
2020-12-10 08:50:18 +0100

    changed some things

--- src/mainScene.js
+++ src/mainScene.js
@@ -1,13 +1,13 @@
 import Phaser from 'phaser';
 import Assets from './Assets/*.png';
 
-export default class mainScene extends Phaser.Scene{
-    constructor(){
-        super({key:"mainScene"});
+export default class mainScene extends Phaser.Scene {
+    constructor() {
+        super({ key: "mainScene" });
     }
-    
 
-    preload(){
+
+    preload() {
         console.table(Assets);
         this.load.image('player', Assets.player);
         this.load.image('enemy', Assets.enemy);
@@ -15,34 +15,34 @@ export default class mainScene extends Phaser.Scene{
 
         this.lane1 = 200;
         this.lane2 = 400;
-        this.lane3 = 600; 
-        this.lane4 = 800; 
-        this.delayTime = 15;
+        this.lane3 = 600;
+        this.lane4 = 800;
+        this.delayTime = 10;
         this.currentDelay = 0;
         this.enemySpawnRate = 120;
         this.currentTimeSpawn = 0;
     }
 
-    create(){
-        
-        this.player = this.add.sprite(this.lane4, 900-256, 'player');
+    create() {
+
+        this.player = this.add.sprite(this.lane4, 900 - 256, 'player');
         this.player.x = this.lane3;
 
         this.cursors = this.input.keyboard.createCursorKeys();
     }
 
-    moveLeft(){
-        if (this.currentDelay == 0){
+    moveLeft() {
+        if (this.currentDelay == 0) {
 
-            if (this.player.x == this.lane2){
+            if (this.player.x == this.lane2) {
                 this.player.x = this.lane1;
 
             }
-            if (this.player.x == this.lane3){
+            if (this.player.x == this.lane3) {
                 this.player.x = this.lane2;
 
             }
-            if (this.player.x == this.lane4){
+            if (this.player.x == this.lane4) {
                 this.player.x = this.lane3;
 
             }
@@ -50,18 +50,18 @@ export default class mainScene extends Phaser.Scene{
         }
     }
 
-    moveRight(){
-        if (this.currentDelay == 0){
-            
-            if (this.player.x == this.lane3){
+    moveRight() {
+        if (this.currentDelay == 0) {
+
+            if (this.player.x == this.lane3) {
                 this.player.x = this.lane4;
 
             }
-            if (this.player.x == this.lane2){
+            if (this.player.x == this.lane2) {
                 this.player.x = this.lane3;
 
             }
-            if (this.player.x == this.lane1){
+            if (this.player.x == this.lane1) {
                 this.player.x = this.lane2;
 
             }
@@ -69,14 +69,14 @@ export default class mainScene extends Phaser.Scene{
         }
     }
 
-    update(time, delta){
-        if (this.cursors.left.isDown){
+    update(time, delta) {
+        if (this.cursors.left.isDown) {
             this.moveLeft();
         }
-        if (this.cursors.right.isDown){
+        if (this.cursors.right.isDown) {
             this.moveRight();
         }
-        if (this.currentDelay > 0){
+        if (this.currentDelay > 0) {
             this.currentDelay--;
         }
         this.enemyAdd();
@@ -84,30 +84,30 @@ export default class mainScene extends Phaser.Scene{
     }
 
     enemyAdd() {
-        if (this.currentTimeSpawn == 0){
+        if (this.currentTimeSpawn == 0) {
 
             var amount = this.getRandomInt(3) + 1;
 
-            for(var i = 0; i < amount; i++){
+            for (var i = 0; i < amount; i++) {
                 var x = this.getRandomInt(4) + 1;
-                if ( x == 1)
-                this.physics.add.image(this.lane4, -256, 'enemy').setVelocity(0, 900);
-                else if ( x == 2)
-                this.physics.add.image(this.lane3, -256, 'enemy').setVelocity(0, 900);
-                else if ( x == 3)
-                this.physics.add.image(this.lane2, -256, 'enemy').setVelocity(0, 900);
-                else 
-                this.physics.add.image(this.lane1, -256, 'enemy').setVelocity(0, 900);
+                if (x == 1)
+                    this.physics.add.image(this.lane4, -256, 'enemy').setVelocity(0, 900);
+                else if (x == 2)
+                    this.physics.add.image(this.lane3, -256, 'enemy').setVelocity(0, 900);
+                else if (x == 3)
+                    this.physics.add.image(this.lane2, -256, 'enemy').setVelocity(0, 900);
+                else
+                    this.physics.add.image(this.lane1, -256, 'enemy').setVelocity(0, 900);
             }
-                this.currentTimeSpawn = this.enemySpawnRate;
+            this.currentTimeSpawn = this.enemySpawnRate;
         }
     }
 
-    powerUpAdd(){
-        
+    powerUpAdd() {
+
     }
 
     getRandomInt(max) {
         return Math.floor(Math.random() * Math.floor(max));
-      }
+    }
 }
